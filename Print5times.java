ebd271e0a18fd2cb48479ef43bfe95b29e898fc1
Deepak <143590207+Deepaks002@users.noreply.github.com>
2025-01-05 06:49:56 -0800

    Add files via upload

---
+++ Print5times.java
@@ -0,0 +1,14 @@
+class Print5times
+{
+	public static void main(String []args)
+	{
+		String name = "Syed Tabrez";
+		int count = 1, end = 5;
+		
+		while (count<=end)
+		{
+			System.out.println(name);
+			count++;
+		}
+	}
+}
